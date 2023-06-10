ee0e942be618a2b987f79e977d702f54a0d26c5e
Scott Adams <scottadams159@gmail.com>
2023-06-10 15:12:45 +0100

    Exercises from redux instead of context

--- components/Exercise/ExerciseBodyPartList.jsx
+++ components/Exercise/ExerciseBodyPartList.jsx
@@ -1,48 +1,20 @@
 import { StyleSheet, FlatList } from "react-native";
-import React, { memo } from "react";
-import Button from "../UI/Button";
-import { GlobalStyles } from "../../constants/styles";
+import { useSelector } from "react-redux";
+import ExerciseBodyPartItem from "./ExerciseBodyPartItem";
 
-const renderBodyPartButton = (item, selectedItem, onSelect) => {
-  return (
-    <Button
-      backgroundColor={
-        item === selectedItem
-          ? GlobalStyles.colors.accent
-          : GlobalStyles.colors.primary
-      }
-      style={styles.button}
-      textStyle={styles.buttonText}
-      onPress={() => onSelect(item)}
-    >
-      {item}
-    </Button>
-  );
-};
+const ExerciseBodyPartList = () => {
+  const bodyParts = useSelector((store) => store.exercises.bodyParts);
 
-const ExerciseBodyPartList = ({ bodyParts, selectedItem, onSelect }) => {
   return (
     <FlatList
       data={bodyParts}
       keyExtractor={(item) => item}
       horizontal={true}
-      renderItem={({ item }) =>
-        renderBodyPartButton(item, selectedItem, onSelect)
-      }
+      renderItem={({ item }) => <ExerciseBodyPartItem item={item} />}
     />
   );
 };
 
 export default ExerciseBodyPartList;
 
-const styles = StyleSheet.create({
-  button: {
-    margin: 3,
-    justifyContent: "center",
-  },
-  buttonText: {
-    fontSize: 11,
-    fontWeight: "normal",
-    textAlign: "center",
-  },
-});
+const styles = StyleSheet.create({});
