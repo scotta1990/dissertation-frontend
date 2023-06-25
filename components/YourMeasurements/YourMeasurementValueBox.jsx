6357dd6214675dfc1c8e71d7579529dbd246b2dc
Scott Adams <scottadams159@gmail.com>
2023-06-25 18:33:07 +0100

    added measurement box and animation

--- components/YourMeasurements/YourMeasurementValueBox.jsx
+++ components/YourMeasurements/YourMeasurementValueBox.jsx
@@ -2,14 +2,44 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { GlobalStyles } from "../../constants/styles";
 
-const YourMeasurementValueBox = ({ title, value, metricType }) => {
+const YourMeasurementValueBox = ({
+  title,
+  value,
+  metric,
+  style,
+  valueTextStyle,
+  titleTextStyle,
+  backgroundColor = GlobalStyles.colors.secondary,
+  textColor = "white",
+  flat = false,
+}) => {
   return (
-    <View style={styles.measurementValueContainer}>
-      <View style={styles.measurementValueTitleContainer}>
-        <Text style={styles.measurementValueTitleText}>{title}</Text>
-      </View>
-      <Text style={styles.measurementValueText}>
-        {value} {metricType}
+    <View
+      style={[
+        styles.measurementValueContainer,
+        !flat && { backgroundColor: backgroundColor },
+        { borderWidth: 2.5, borderColor: backgroundColor },
+        style,
+      ]}
+    >
+      <Text
+        style={[
+          styles.measurementValueText,
+          { color: flat ? backgroundColor : textColor },
+          valueTextStyle,
+        ]}
+      >
+        {value}
+        {metric}
+      </Text>
+      <Text
+        style={[
+          styles.measurementValueTitleText,
+          { color: flat ? backgroundColor : textColor },
+          titleTextStyle,
+        ]}
+      >
+        {title}
       </Text>
     </View>
   );
@@ -19,29 +49,18 @@ export default YourMeasurementValueBox;
 
 const styles = StyleSheet.create({
   measurementValueContainer: {
-    backgroundColor: GlobalStyles.colors.secondary3 + "70",
     flex: 1,
     margin: 10,
-    padding: 5,
+    padding: 8,
     alignItems: "center",
-    borderColor: GlobalStyles.colors.primary,
-    borderWidth: 0.75,
+    justifyContent: "center",
     borderRadius: 8,
   },
-  measurementValueTitleContainer: {
-    width: "90%",
-    justifyContent: "center",
-    alignItems: "center",
-    borderBottomColor: GlobalStyles.colors.primary,
-    borderBottomWidth: 0.5,
+  measurementValueText: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
   measurementValueTitleText: {
-    fontSize: 10,
-    color: GlobalStyles.colors.primary2,
-  },
-  measurementValueText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: GlobalStyles.colors.primary,
+    fontSize: 12,
   },
 });
