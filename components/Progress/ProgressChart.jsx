dbc9edb20d6ac64150e0de2947f3fcd37afef81d
Scott Adams <scottadams159@gmail.com>
2023-07-10 18:06:10 +0100

    adding test data viz

---
+++ components/Progress/ProgressChart.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ProgressChart = () => {
+  return (
+    <View>
+      <Text>ProgressChart</Text>
+    </View>
+  );
+};
+
+export default ProgressChart;
+
+const styles = StyleSheet.create({});
---
+++ temporary-chart-data.js
@@ -0,0 +1,218 @@
+export const tempChartData = [
+  {
+    Date: {
+      $date: "2023-06-01T00:00:00.000Z",
+    },
+    DateString: "23-06-01",
+  },
+  {
+    Date: {
+      $date: "2023-06-02T00:00:00.000Z",
+    },
+    DateString: "23-06-02",
+  },
+  {
+    Date: {
+      $date: "2023-06-03T00:00:00.000Z",
+    },
+    DateString: "23-06-03",
+  },
+  {
+    Date: {
+      $date: "2023-06-04T00:00:00.000Z",
+    },
+    DateString: "23-06-04",
+  },
+  {
+    Date: {
+      $date: "2023-06-05T00:00:00.000Z",
+    },
+    DateString: "23-06-05",
+  },
+  {
+    Date: {
+      $date: "2023-06-06T00:00:00.000Z",
+    },
+    DateString: "23-06-06",
+  },
+  {
+    Date: {
+      $date: "2023-06-07T00:00:00.000Z",
+    },
+    DateString: "23-06-07",
+  },
+  {
+    Date: {
+      $date: "2023-06-08T00:00:00.000Z",
+    },
+    DateString: "23-06-08",
+  },
+  {
+    Date: {
+      $date: "2023-06-09T00:00:00.000Z",
+    },
+    DateString: "23-06-09",
+  },
+  {
+    Date: {
+      $date: "2023-06-10T00:00:00.000Z",
+    },
+    DateString: "23-06-10",
+  },
+  {
+    Date: {
+      $date: "2023-06-11T00:00:00.000Z",
+    },
+    DateString: "23-06-11",
+  },
+  {
+    Date: {
+      $date: "2023-06-12T00:00:00.000Z",
+    },
+    DateString: "23-06-12",
+  },
+  {
+    Date: {
+      $date: "2023-06-13T00:00:00.000Z",
+    },
+    DateString: "23-06-13",
+  },
+  {
+    Date: {
+      $date: "2023-06-14T00:00:00.000Z",
+    },
+    DateString: "23-06-14",
+  },
+  {
+    Date: {
+      $date: "2023-06-15T00:00:00.000Z",
+    },
+    DateString: "23-06-15",
+  },
+  {
+    Date: {
+      $date: "2023-06-16T00:00:00.000Z",
+    },
+    DateString: "23-06-16",
+  },
+  {
+    Date: {
+      $date: "2023-06-17T00:00:00.000Z",
+    },
+    DateString: "23-06-17",
+  },
+  {
+    Date: {
+      $date: "2023-06-18T00:00:00.000Z",
+    },
+    DateString: "23-06-18",
+  },
+  {
+    Date: {
+      $date: "2023-06-19T00:00:00.000Z",
+    },
+    DateString: "23-06-19",
+  },
+  {
+    Date: {
+      $date: "2023-06-20T00:00:00.000Z",
+    },
+    DateString: "23-06-20",
+  },
+  {
+    _id: {
+      $oid: "6494a5eaee65cee5eed097bf",
+    },
+    measurementTypeId: {
+      $oid: "649344043a65421e6b78128a",
+    },
+    value: 21,
+    dateCreated: {
+      $date: "2023-06-21T19:49:02.062Z",
+    },
+    userId: {
+      $oid: "648f7e06cd7de38a5078b656",
+    },
+    __v: 0,
+    DateString: "23-06-21",
+    Date: {
+      $date: "2023-06-21T00:00:00.000Z",
+    },
+  },
+  {
+    _id: {
+      $oid: "6494b4bfa089aa4b61d28601",
+    },
+    measurementTypeId: {
+      $oid: "649344043a65421e6b78128a",
+    },
+    value: 16,
+    dateCreated: {
+      $date: "2023-06-22T20:26:13.801Z",
+    },
+    userId: {
+      $oid: "648f7e06cd7de38a5078b656",
+    },
+    __v: 0,
+    DateString: "23-06-22",
+    Date: {
+      $date: "2023-06-22T00:00:00.000Z",
+    },
+  },
+  {
+    _id: {
+      $oid: "6495ce591d2001219452e1c5",
+    },
+    measurementTypeId: {
+      $oid: "649344043a65421e6b78128a",
+    },
+    value: 17,
+    dateCreated: {
+      $date: "2023-06-23T15:07:57.129Z",
+    },
+    userId: {
+      $oid: "648f7e06cd7de38a5078b656",
+    },
+    __v: 0,
+    DateString: "23-06-23",
+    Date: {
+      $date: "2023-06-23T00:00:00.000Z",
+    },
+  },
+  {
+    Date: {
+      $date: "2023-06-24T00:00:00.000Z",
+    },
+    DateString: "23-06-24",
+  },
+  {
+    Date: {
+      $date: "2023-06-25T00:00:00.000Z",
+    },
+    DateString: "23-06-25",
+  },
+  {
+    Date: {
+      $date: "2023-06-26T00:00:00.000Z",
+    },
+    DateString: "23-06-26",
+  },
+  {
+    Date: {
+      $date: "2023-06-27T00:00:00.000Z",
+    },
+    DateString: "23-06-27",
+  },
+  {
+    Date: {
+      $date: "2023-06-28T00:00:00.000Z",
+    },
+    DateString: "23-06-28",
+  },
+  {
+    Date: {
+      $date: "2023-06-29T00:00:00.000Z",
+    },
+    DateString: "23-06-29",
+  },
+];
