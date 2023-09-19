4db2c6f7f11fb23febec0178827a64d05ca42f39
scotta1990 <36856468+scotta1990@users.noreply.github.com>
2023-09-19 18:14:21 +0100

    add removed files

---
+++ utils/auth.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { baseUrl } from "./database/config";
+
+const authenticate = async (mode, email, password) => {
+  const url = `${baseUrl}/user/${mode}`;
+
+  return await axios.post(
+    url,
+    {
+      email: email,
+      password: password,
+    },
+    { timeout: 9000 }
+  );
+};
+
+export const createUser = async (email, password) => {
+  return await authenticate("register", email, password);
+};
+
+export const login = async (email, password) => {
+  return await authenticate("login", email, password);
+};
---
+++ utils/database/config.js
@@ -0,0 +1,1 @@
+export const baseUrl = `API_URL`;
