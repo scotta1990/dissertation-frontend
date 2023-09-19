fddf85991c8d24b3ee30ddafadd75048c6dcb1af
scotta1990 <36856468+scotta1990@users.noreply.github.com>
2023-09-19 18:37:49 +0100

    correct

--- store/redux/auth.js
+++ store/redux/auth.js
@@ -1,28 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-export const authSlice = createSlice({
-  name: "auth",
-  initialState: {
-    token: null,
-    isAuthenticated: false,
-  },
-  reducers: {
-    authenticate: (state, action) => {
-      state.token = action.payload.token;
-      state.isAuthenticated = true;
-    },
-
-    logout: (state, action) => {
-      state.token = null;
-      state.isAuthenticated = false;
-    },
-  },
-});
-
-export const { authenticate, logout } = authSlice.actions;
-export default authSlice.reducer;
-
-import { createSlice } from "@reduxjs/toolkit";
 import jwtDecode from "jwt-decode";
 
 export const authSlice = createSlice({
